e631e7ef6bcc8dec0b44c225767f022b475e572d
LEE-NS <116232576+LEE-NS@users.noreply.github.com>
2023-04-02 17:07:13 +0900

    Delete main.js

--- Class_pushToGithub/js_g/main.js
+++
@@ -1,6 +0,0 @@
-const getLatestNews = () => {
-    let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=US&topic=business`);
-    let header = new Headers({'x-api-key' : 'GMvGtjkYMSdUosPpVNe6pIM2LAiM7Oc4PLgBB2m8'});
-    let response = fetch(url, {headers: header});
-};
-getLatestNews();

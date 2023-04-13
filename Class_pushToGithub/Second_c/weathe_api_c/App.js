d5a3f22f62ca5cbf46bd27d59563551e838b3eb7
LEE-NS <116232576+LEE-NS@users.noreply.github.com>
2023-04-13 20:58:34 +0900

    Update App.js

--- Class_pushToGithub/Second_c/weathe_api_c/App.js
+++ Class_pushToGithub/Second_c/weathe_api_c/App.js
@@ -7,7 +7,7 @@ import './App.css';
 function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
-  const cities = ['Busan', 'Seoul', 'Daegu', 'Jinju'];
+  const cities = ['Busan', 'Seoul', 'Daegu', 'Incheon'];
   
   const getCurrentLocation = () => {
     navigator.geolocation.watchPosition((position) => {

0ec7fb34f05e4333eacd5fab18e5e00169834596
LEE-NS <116232576+LEE-NS@users.noreply.github.com>
2023-04-19 14:53:55 +0900

    Add files via upload

---
+++ Class_pushToGithub/weather_api/Components/WeatherButtons.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap'
+
+const WeatherButtons = ({cities, city, setCity, handleCityChange}) => {
+
+  return (
+    <div>
+        <Button variant={`${city == '' ? 'Primary' : 'outline-primary'}`} onClick={() => handleCityChange('current')}>📍 내 위치</Button>
+        {cities.map((item) => {
+          return (
+          <Button variant={`${city == item ? 'Secondary' : 'outline-secondary'}`} onClick={() => setCity(item)}>
+            {item}
+          </Button>
+          );
+        })}
+    </div>
+  )
+}
+
+export default WeatherButtons
+
+// App.js(중앙 컴포넌트)에서 각 컴포넌트에 분배하게 될 변수와 함수들을 모두 통제할 수 있게 해야한다.

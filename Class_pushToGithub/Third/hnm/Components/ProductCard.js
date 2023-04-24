63da836d076ff640d19aa4a6883a2b3cdb5e610e
LEE-NS <116232576+LEE-NS@users.noreply.github.com>
2023-04-24 22:25:42 +0900

    Update ProductCard.js

--- Class_pushToGithub/Third/hnm/Components/ProductCard.js
+++ Class_pushToGithub/Third/hnm/Components/ProductCard.js
@@ -5,7 +5,7 @@ const ProductCard = () => {
     <div>
         <img src='https://lp2.hm.com/hmgoepprod?set=source[/63/df/63df7d14611a105208a2b04b4665448ee85f3183.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[2]&call=url[file:/product/main]' />
         <div>Conscious Choice</div>
-        <div>벨티드 트윌 코트</div>
+        <div>제품 이름 테스트</div>
         <div>&#8361;99900</div>
         <div>신제품</div>
     </div>

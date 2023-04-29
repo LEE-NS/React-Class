37e60e78b8d0e07e989aa7fdb41a813b68677e2a
LEE-NS <116232576+LEE-NS@users.noreply.github.com>
2023-04-29 13:46:29 +0900

    Update ProductCard.js

--- Class_pushToGithub/Third/hnm/Components/ProductCard.js
+++ Class_pushToGithub/Third/hnm/Components/ProductCard.js
@@ -21,5 +21,5 @@ usestate를 통해 db 안의 정보를 나눠 줄 변수 지정, set함수에는
 
 ProductCard 컴포넌트로 db를 읽어올 변수를 props로 수입한다
 
-db의 배열의 index를 뒤집은 다음 map을 걸어 pop으로 db 안의 product 배열의 각 개체에 대한 key값을 하나씩 해당 위치에 배치
+db의 배열의 index를 뒤집은 다음 map을 걸어 pop으로 db 안의 product 배열의 각 개체에 대한 key에 해당하는 value를 각각 해당 위치에 배치
 */

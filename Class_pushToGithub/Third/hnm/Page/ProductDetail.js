d9ab67fbb68fdf91f1902f5740ef3b55b9cf3c4d
LEE-NS <116232576+LEE-NS@users.noreply.github.com>
2023-05-06 16:26:53 +0900

    Update ProductDetail.js

--- Class_pushToGithub/Third/hnm/Page/ProductDetail.js
+++ Class_pushToGithub/Third/hnm/Page/ProductDetail.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row, Dropdown, Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
   let {id} = useParams();
   const [product, setProduct] = useState(null);
   const getProductDetail = async () => {
-    let url = `http://localhost:3500/products/${id}`;
+    let url = `https://my-json-server.typicode.com/LEE-NS/front-hnm/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setProduct(data);
@@ -22,9 +22,23 @@ const ProductDetail = () => {
         <Col className='product-dtl-img'>
           <img src={product?.img} />
         </Col>
-        <Col>
-          <div>{product?.title}</div>
-          <div>￦{product?.price}</div>
+        <Col className='product-dtl-info'>
+          <div className='info-t'>{product?.title}</div>
+          <div className='info-p'>￦{product?.price}</div>
+          <div className='info-ch'>{product?.choice == true ? "Conscious Choice" : ""}</div>
+          <Dropdown className='dd'>
+            <Dropdown.Toggle variant="outline-dark" id="dropdown-basic">
+              사이즈 선택
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item>{product?.size[0]}</Dropdown.Item>
+              <Dropdown.Item>{product?.size[1]}</Dropdown.Item>
+              <Dropdown.Item>{product?.size[2]}</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <div className='d-grid gap-1'>
+            <Button variant="dark" className='btn'>장바구니에 담기</Button>
+          </div>
         </Col>
       </Row>
     </Container>
